412508c5db317c856020d79c80502946584a00a2
zhanglianxiang <zhanglianxiang@goyoo.com>
2015-12-16 17:53:12 +0800

    use martini web frame

--- src/router/router.go
+++ src/router/router.go
@@ -3,66 +3,73 @@ package router
 import (
 	"handlers"
 
-	"github.com/drone/routes"
+	//	"github.com/drone/routes"
+
+	"github.com/go-martini/martini"
 )
 
-var Mux *routes.RouteMux
+var Routers martini.Router
 
 func init() {
-	Mux = routes.New()
+	Routers = martini.NewRouter()
 	/* method GET */
 	/* jenkins */
-	Mux.Get("/api/info", handlers.HandlerGetInfo)
+	Routers.Get("/api/info", handlers.HandlerGetInfo)
+	/* login */
+	Routers.Get("/login", handlers.HandlerLogin)
+	Routers.Post("/login", handlers.HandlerLogin)
+	/* dashboard */
+	Routers.Get("/dashboard", handlers.HandlerDashboard)
 	/* job */
-	Mux.Get("/", handlers.HandlerDefault)
-	Mux.Get("/api/job/alljobs", handlers.HandlerGetAllJobs)
-	Mux.Get("/api/job/ajob/:jobid", handlers.HandlerGetJob)
-	Mux.Get("/api/job/isrunning/:jobid", handlers.HandlerJobRunning)
-	Mux.Get("/api/job/:jobid/allbuilds", handlers.HandlerGetAllBuildIds)
-	Mux.Get("/api/job/config/:jobid", handlers.HandlerJobConfig)
-	Mux.Get("/api/job/buildlog/:jobid/:number", handlers.HandlerBuildConsoleOutput)
+	Routers.Get("/", handlers.HandlerDefault)
+	Routers.Get("/api/job/alljobs", handlers.HandlerGetAllJobs)
+	Routers.Get("/api/job/ajob/:jobid", handlers.HandlerGetJob)
+	Routers.Get("/api/job/isrunning/:jobid", handlers.HandlerJobRunning)
+	Routers.Get("/api/job/:jobid/allbuilds", handlers.HandlerGetAllBuildIds)
+	Routers.Get("/api/job/config/:jobid", handlers.HandlerJobConfig)
+	Routers.Get("/api/job/buildlog/:jobid/:number", handlers.HandlerBuildConsoleOutput)
 	/* node */
-	Mux.Get("/api/node/allnodes", handlers.HandlerGetAllNodes)
-	Mux.Get("/api/node/anode/:nodeid", handlers.HandlerGetNode)
-	Mux.Get("/api/node/nodeinfo/:nodeid", handlers.HandlerNodeInfo)
-	Mux.Get("/isjnlpagent/:nodeid", handlers.HandlerIsJnlpAgent)
-	Mux.Get("/api/node/isidle/:nodeid", handlers.HandlerIsIdle)
-	Mux.Get("/api/node/isonline/:nodeid", handlers.HandlerIsOnline)
-	Mux.Get("/api/node/log/:nodeid", handlers.HandlerGetLogText)
+	Routers.Get("/api/node/allnodes", handlers.HandlerGetAllNodes)
+	Routers.Get("/api/node/anode/:nodeid", handlers.HandlerGetNode)
+	Routers.Get("/api/node/nodeinfo/:nodeid", handlers.HandlerNodeInfo)
+	Routers.Get("/isjnlpagent/:nodeid", handlers.HandlerIsJnlpAgent)
+	Routers.Get("/api/node/isidle/:nodeid", handlers.HandlerIsIdle)
+	Routers.Get("/api/node/isonline/:nodeid", handlers.HandlerIsOnline)
+	Routers.Get("/api/node/log/:nodeid", handlers.HandlerGetLogText)
 	/* queue */
-	Mux.Get("/queue", handlers.HandlerGetQueue)
-	Mux.Get("/queueurl", handlers.HandlerGetQueueUrl)
+	Routers.Get("/queue", handlers.HandlerGetQueue)
+	Routers.Get("/queueurl", handlers.HandlerGetQueueUrl)
 	/* view */
-	Mux.Get("/api/view/allviews", handlers.HandlerGetAllViews)
-	Mux.Get("/api/view/:viewid", handlers.HandlerGetView)
+	Routers.Get("/api/view/allviews", handlers.HandlerGetAllViews)
+	Routers.Get("/api/view/:viewid", handlers.HandlerGetView)
 	/* plugin */
-	Mux.Get("/api/plugins/:depth", handlers.HandlerGetPlugins)
+	Routers.Get("/api/plugins/:depth", handlers.HandlerGetPlugins)
 	/* log */
-	Mux.Get("/api/log/level", handlers.HandlerGetLogLevel)
+	Routers.Get("/api/log/level", handlers.HandlerGetLogLevel)
 	/* build */
-	//Mux.Get("/api///")
+	//Routers.Get("/api///")
 	/* method POST */
 	/* job */
-	Mux.Post("/api/job/create/:jobid", handlers.HandlerCreateJob)
-	Mux.Post("/api/job/delete/:jobid", handlers.HandlerDeleteJob)
-	Mux.Post("/api/job/disable/:jobid", handlers.HandlerDisableJob)
-	Mux.Post("/api/job/enable/:jobid", handlers.HandlerEnableJob)
-	Mux.Post("/api/job/rename/:oldjobid/:newjobid", handlers.HandlerRenameJob)
-	Mux.Post("/api/job/build/:jobid", handlers.HandlerBuildJob)
-	Mux.Post("/api/job/stopbuild/:jobid/:number", handlers.HandlerStopBuild)
-	//Mux.Post("/api/job/copy/:from/:newname", handlers.HandlerCopyJob)
+	Routers.Post("/api/job/create/:jobid", handlers.HandlerCreateJob)
+	Routers.Post("/api/job/delete/:jobid", handlers.HandlerDeleteJob)
+	Routers.Post("/api/job/disable/:jobid", handlers.HandlerDisableJob)
+	Routers.Post("/api/job/enable/:jobid", handlers.HandlerEnableJob)
+	Routers.Post("/api/job/rename/:oldjobid/:newjobid", handlers.HandlerRenameJob)
+	Routers.Post("/api/job/build/:jobid", handlers.HandlerBuildJob)
+	Routers.Post("/api/job/stopbuild/:jobid/:number", handlers.HandlerStopBuild)
+	//Routers.Post("/api/job/copy/:from/:newname", handlers.HandlerCopyJob)
 	/* view */
-	Mux.Post("/api/view/create/:name/:type", handlers.HandlerCreateView)
-	Mux.Post("/api/view/addjob/:name/:jobid", handlers.HandlerViewAddJob)
-	Mux.Post("/api/view/deletejob/:name/:jobid", handlers.HandlerViewDeleteJob)
+	Routers.Post("/api/view/create/:name/:type", handlers.HandlerCreateView)
+	Routers.Post("/api/view/addjob/:name/:jobid", handlers.HandlerViewAddJob)
+	Routers.Post("/api/view/deletejob/:name/:jobid", handlers.HandlerViewDeleteJob)
 	/* node */
-	Mux.Post("/api/node/create/:nodeid/:numexecutors/:description/:remotefs", handlers.HandlerAddNode)
-	Mux.Post("/api/node/delete/:nodeid", handlers.HandlerDeleteNode)
-	//Mux.Get("/toggletempoffline/:nodeid", handlers.HandlerNodeToggleTemporarilyOffline)
-	Mux.Post("/api/node/online/:nodeid", handlers.HandlerSetOnline)
-	Mux.Post("/api/node/offline/:nodeid", handlers.HandlerSetOffline)
-	Mux.Post("/api/node/launch/:nodeid", handlers.HandlersLaunchNode)
-	Mux.Post("/api/node/disconnect/:nodeid", handlers.HandlerDisconnect)
+	Routers.Post("/api/node/create/:nodeid/:numexecutors/:description/:remotefs", handlers.HandlerAddNode)
+	Routers.Post("/api/node/delete/:nodeid", handlers.HandlerDeleteNode)
+	//Routers.Get("/toggletempoffline/:nodeid", handlers.HandlerNodeToggleTemporarilyOffline)
+	Routers.Post("/api/node/online/:nodeid", handlers.HandlerSetOnline)
+	Routers.Post("/api/node/offline/:nodeid", handlers.HandlerSetOffline)
+	Routers.Post("/api/node/launch/:nodeid", handlers.HandlersLaunchNode)
+	Routers.Post("/api/node/disconnect/:nodeid", handlers.HandlerDisconnect)
 	/* log */
-	Mux.Post("/api/log/set/:level", handlers.HandlerSetLogLevel)
+	Routers.Post("/api/log/set/:level", handlers.HandlerSetLogLevel)
 }
--- src/server.go
+++ src/server.go
@@ -3,12 +3,15 @@ package main
 import (
 	"net/http"
 	"router"
-	"runtime"
+
+	"github.com/go-martini/martini"
 )
 
 func main() {
-	runtime.GOMAXPROCS(runtime.NumCPU())
-	http.Handle("/", router.Mux)
-	http.ListenAndServe(":3000", nil)
-	return
+	//	runtime.GOMAXPROCS(runtime.NumCPU())
+	m := martini.New()
+	m.MapTo(router.Routers, (*martini.Routes)(nil))
+	m.Action(router.Routers.Handle)
+	http.Handle("/", m)
+	m.RunOnAddr(":3000")
 }
