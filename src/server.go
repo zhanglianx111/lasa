1532fe1c5515a3f508477c966addf19842c7a023
zhanglianxiang <zhanglianxiang@goyoo.com>
2015-12-21 20:11:46 +0800

    update server.go

--- src/server.go
+++ src/server.go
@@ -10,8 +10,12 @@ import (
 func main() {
 	//	runtime.GOMAXPROCS(runtime.NumCPU())
 	m := martini.New()
+	// for static files service to dir static
+	m.Use(martini.Static("static"))
+	// for router
 	m.MapTo(router.Routers, (*martini.Routes)(nil))
 	m.Action(router.Routers.Handle)
 	http.Handle("/", m)
+	// run app at port 3000
 	m.RunOnAddr(":3000")
 }
