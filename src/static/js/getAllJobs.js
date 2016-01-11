c40cf225e55441449c2074a2632a1037602c13d6
Evilcome <evilcome.com@gmail.com>
2016-01-11 17:26:08 +0800

    add two function

--- src/static/js/getAllJobs.js
+++ src/static/js/getAllJobs.js
@@ -14,6 +14,48 @@ $(document).ready(function () {
 		addrows(data);
 	};
 
+	function renderDropDown(id, data) {
+		// 你可以得到传进来的 id 和 data 来用
+
+		return '<div class="dropdown">' +
+
+			// 这地方 id 不能写死，可以把 id 去掉
+			'<button class="btn dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown">' +
+				'Action <span class="caret"></span>' +
+			'</button>' +
+
+			// ul list
+			'<ul class="dropdown-menu text-right" role="menu" aria-labelledby="dropdownMenu1">' +
+				 '<li role="presentation"><a role="menuitem" tabindex="-1" id="build" onclick="mybuild(this)">Build</a></li>' +
+				 '<li role="presentation"><a role="menuitem" tabindex="-1" onclick="buildlog(this)">Log</a></li>' +
+				 '<li role="presentation"><a role="menuitem" tabindex="-1" onclick="mydelete(this)">Delete</a></li>' +
+				 '<li role="presentation" class="divider"></li>' +
+				 '<li role="presentation"><a role="menuitem" tabindex="-1" onclick="jobconfig(this)">Job Configuration</a></li>' +
+			'</ul>' +
+
+			'</div>';
+	}
+
+	function renderRow(data) {
+		var $tableBody = $('tbody');
+
+		var content = '';
+		for(var i = 0, len = data.length; i != len; i++) {
+			var each = data[i];
+			var trId = i + 1;
+
+			content += '<tr data-id="' + trId + '">' +
+				'<td>' + each.id + '</td>' +
+				'<td>' + each.name + '</td>' +
+				'<td>' + each.name + '</td>' +
+				'<td>' + renderDropDown(trId, each) + '</td>' +
+				'<td><button class="btn btn-sm btn-warning"><span></span>process...</button></td>' +
+				'</tr>';
+		}
+
+		$tableBody.html(content);
+	}
+
 	function addrows(data)
 	{
 		var dataLength = data.length;
@@ -66,7 +108,7 @@ $(document).ready(function () {
   eUl.className = "dropdown-menu text-right";
   eUl.setAttribute("role","menu");
   eUl.setAttribute("aria-labelledby","dropdownMenu1");
-  
+
   var eLiA = document.createElement("li");
   eLiA.setAttribute("role", "presentation");
 
